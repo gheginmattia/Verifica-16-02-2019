7d916d6be70c3366205452fe165f696c24f81773
gheginmattia <46593826+gheginmattia@users.noreply.github.com>
2019-02-16 11:33:13 +0100

    Add files via upload

--- Verifica_16.02.19/Verifica_16.02.19.ino
+++ Verifica_16.02.19/Verifica_16.02.19.ino
@@ -2,11 +2,12 @@ int led_giallo=6;
 int led_bianco=8;
 int led_verde=10;
 int led_rosso=12;
-int rosso_giallo=0;
-int bianco_verde=0;  
+int aspetta=0; 
 int serial_begin=9600;
 int i=0;
 int giri=0;
+int minimo=0;
+int massimo=0;
 void rg (void);
 void bv (void);
 void ripeti (void);
@@ -16,30 +17,32 @@ void setup() {
   pinMode(led_verde, OUTPUT);
   pinMode(led_rosso, OUTPUT);
   Serial.begin(serial_begin);
-  Serial.println("Inserisci la durata di rosso e giallo in millisecondi");
-  while(Serial.available()==0){}
-  rosso_giallo=Serial.readString().toInt();
-  Serial.println(rosso_giallo);
-  Serial.println("Inserisci la durata di bianco e verde in millisecondi");
-  while(Serial.available()==0){}
-  bianco_verde=Serial.readString().toInt();
-  Serial.println(bianco_verde);
   Serial.println("Inserisci quanti giri fare prima di fermarsi");
   while(Serial.available()==0){}
   giri=Serial.readString().toInt();
   Serial.println(giri);
+  Serial.println("Inserisci il minimo valore del random");
+  while(Serial.available()==0){}
+  minimo=Serial.readString().toInt();
+  Serial.println(minimo);
+  Serial.println("Inserisci il massimo valore del random");
+  while(Serial.available()==0){}
+  massimo=Serial.readString().toInt();
+  Serial.println(massimo);
+  aspetta = random(minimo,massimo);
+  Serial.println(aspetta);
   } 
   void rg (void){
     digitalWrite(led_rosso,HIGH);
     digitalWrite(led_giallo,HIGH);
-    delay(rosso_giallo);
+    delay(aspetta);
     digitalWrite(led_rosso,LOW);
     digitalWrite(led_giallo,LOW);
   }
   void bv (void) {
     digitalWrite(led_bianco,HIGH);
     digitalWrite(led_verde,HIGH);
-    delay(bianco_verde);
+    delay(aspetta);
     digitalWrite(led_bianco,LOW);
     digitalWrite(led_verde,LOW);
   }
